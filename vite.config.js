165fc1bcdd841cc88b8f8cedd79a0e3e3be6ec6c
Ahmed Elshazly <57854391+Shazly99@users.noreply.github.com>
2025-02-08 14:49:44 +0200

    Add Vercel configuration and clean up Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,8 @@ import jsconfigPaths from 'vite-jsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), jsconfigPaths()],
-  server: {
-    proxy: {
-      '/v1.1': { // أي طلب يبدأ بـ /v1.1 سيتم توجيهه إلى ZeptoMail
-        target: 'https://api.zeptomail.com', // الرابط الأساسي لـ API
-        changeOrigin: true, // يغير الأصل ليطابق الهدف
-        secure: true, // تعيينه إلى false إذا كنت تستخدم HTTPS مع شهادة غير موثوقة
-      },
-    },
-  },
-  resolve: {
+  plugins: [react(),svgr(),jsconfigPaths()],
+    resolve: {
     alias: {
       '@': '/src'
     }
